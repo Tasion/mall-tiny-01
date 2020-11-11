6be6f1f0fd65f48758e7f73f68537732b995716d
Tasion <tasons@163.com>
2020-11-11 16:28:49 +0800

    mall整合Redis实现缓存功能

---
+++ src/main/java/com/macro/mall/tiny/controller/UmsMemberController.java
@@ -0,0 +1,30 @@
+package com.macro.mall.tiny.controller;
+
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.service.UmsMemberService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class UmsMemberController {
+    @Autowired
+    private UmsMemberService memberService;
+
+    @ApiOperation("获取验证码")
+    @RequestMapping(value = "/getAuthCode", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return memberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation("判断验证码是否正确")
+    @RequestMapping(value = "/verifyAuthCode", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updatePassword(@RequestParam String telephone,
+                                       @RequestParam String authCode) {
+        return memberService.verifyAuthCode(telephone,authCode);
+    }
+}
