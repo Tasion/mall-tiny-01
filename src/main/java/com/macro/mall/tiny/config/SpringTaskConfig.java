afc78f920a02876435daaa7919aa1ce62a39a9e6
Tasion <tasons@163.com>
2020-11-15 19:50:41 +0800

    mall整合SpringTask实现定时任务

---
+++ src/main/java/com/macro/mall/tiny/config/SpringTaskConfig.java
@@ -0,0 +1,12 @@
+package com.macro.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 任务定时配置
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
